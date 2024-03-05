4a322bbe729834b0e1fcb4dc1a477e420be8cf76
michelrbx7 <160605718+michelrbx7@users.noreply.github.com>
2024-03-05 08:33:54 -0300

    Add files via upload

---
+++ números impares entre 1 e 15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+int i;
+
+printf("\nLista de todos os numero Impares entre 1 e 15:");
+
+for(i=1; i<=15;i++){
+	if(i%2==1)
+   printf("%d \n",i);
+   	
+ }
+ return 0;	
+}	
+	
+
