aabb1aece750e53fbd30b5a8e39a0bc323d44499
michelrbx7 <160605718+michelrbx7@users.noreply.github.com>
2024-03-05 11:05:31 -0300

    Add files via upload

---
+++ solicitar 5 numero e somar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void cabecalho() {
+ system ("cls || clear");
+ printf("====SENAI=====");
+ fflush(stdin);
+}
+int main(){
+setlocale(LC_ALL,"");
+int nota,soma,i;
+
+//solicitando dados para o usuario.
+for(i =1; i<=5; i++){
+ printf("Digite a %iª nota",i);
+ scanf("%i",&nota);
+ 
+ soma=soma + nota;	
+}
+printf("\nSoma:%i \n",soma);
+	
+	
+	
+	
+		
+	
+return 0;	
+}
+
