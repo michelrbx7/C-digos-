4214423e48da4b570c6ef07ed2218ffe5c22b35a
michelrbx7 <160605718+michelrbx7@users.noreply.github.com>
2024-03-05 09:21:17 -0300

    Add files via upload

--- soma de todosos numeros.c
+++ soma de todosos numeros.c
@@ -1,45 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
- void cabecalho() {
+#include <locale.h>
+
+void cabecalho() {
  system ("cls || clear");
  printf("====SENAI=====");
+ fflush(stdin);
 }
-//declarando variaveis
 int main(){
-int primeiroNumero,segundoNumero,terceiroNumero,quartoNumero,quintoNumero;
-float soma;
+setlocale(LC_ALL,"");
+int nota,soma,i;
 
-//solicitando dados ao usuario
- cabecalho();
-printf("\nDigite um numero:");
-scanf("%i",&primeiroNumero);
-
-printf("\nDigite um numero:");
-scanf("%i",&segundoNumero);
-
-printf("\nDigite um numero:");
-scanf("%i",&terceiroNumero);
-
-printf("\nDigite um numero:");
-scanf("%i",&quartoNumero);
-
-printf("\nDigite um numero:");
-scanf("%i",&quintoNumero);	
-
-soma=(primeiroNumero + segundoNumero +terceiroNumero + quartoNumero+ quintoNumero);
-
- cabecalho();
- printf("\n======Exibindo Resultados======");
- printf("\nPrimeiro Numero:%.1i",primeiroNumero);
- printf("\nSegundo Numero:%.1i",segundoNumero);
- printf("\nTerceiro Numero:%.1i",terceiroNumero);
- printf("\nQuarto Numero:%.1i",quartoNumero);
- printf("\nQuinto Numero:%.1i",quintoNumero);
- printf("\nSoma=%.1f\n",soma);
+//solicitando dados para o usuario.
+for(i =1; i<=5; i++){
+ printf("Digite a %iª nota",i);
+ scanf("%i",&nota);
  
- 
+ soma=soma + nota;	
+
+printf("\nSoma:%i \n",soma);
+}	
+	
+	
 	
 	
 	
 return 0;	
 }
+
